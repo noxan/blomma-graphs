3bbde4dfdb6631c2aa79a3e1243c2f4c76ceb0ae
noxan <noxan@byteweaver.net>
2013-08-28 15:39:20 +0200

    add dummy values (not null) to stg serializer methods

--- src/com/github/noxan/blommagraphs/serializer/STGSerializer.java
+++ src/com/github/noxan/blommagraphs/serializer/STGSerializer.java
@@ -1,6 +1,7 @@
 package com.github.noxan.blommagraphs.serializer;
 
 
+import com.github.noxan.blommagraphs.graphs.DefaultTaskGraph;
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
 
 
@@ -8,12 +9,12 @@ public class STGSerializer implements TaskGraphSerializer {
     @Override
     public String serialize(TaskGraph graph) {
         // TODO Auto-generated method stub
-        return null;
+        return "";
     }
 
     @Override
     public TaskGraph deserialize(String graphString) {
         // TODO Auto-generated method stub
-        return null;
+        return new DefaultTaskGraph();
     }
 }
