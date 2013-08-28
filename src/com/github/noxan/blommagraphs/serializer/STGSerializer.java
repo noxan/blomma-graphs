42bd371ff9c45991913fa6237eb2e0d4d6c055e0
noxan <noxan@byteweaver.net>
2013-08-28 13:26:15 +0200

    add dummy implementation of TaskGraphSerializer named STGSerializer

---
+++ src/com/github/noxan/blommagraphs/serializer/STGSerializer.java
@@ -0,0 +1,19 @@
+package com.github.noxan.blommagraphs.serializer;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+
+
+public class STGSerializer implements TaskGraphSerializer {
+    @Override
+    public String serialize(TaskGraph graph) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TaskGraph deserialize(String graphString) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
