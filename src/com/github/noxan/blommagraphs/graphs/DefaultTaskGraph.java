fb0292fc274643201811e35150499868fb96f8dc
noxan <noxan@byteweaver.net>
2013-08-28 13:21:52 +0200

    create taskgraph interface and defaulttaskgraph class

---
+++ src/com/github/noxan/blommagraphs/graphs/DefaultTaskGraph.java
@@ -0,0 +1,5 @@
+package com.github.noxan.blommagraphs.graphs;
+
+
+public class DefaultTaskGraph implements TaskGraph {
+}
---
+++ src/com/github/noxan/blommagraphs/graphs/TaskGraph.java
@@ -0,0 +1,5 @@
+package com.github.noxan.blommagraphs.graphs;
+
+
+public interface TaskGraph {
+}
