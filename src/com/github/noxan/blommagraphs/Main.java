50fcc88b2fc3adc659af1c04ec31cc10fca0dbac
noxan <noxan@byteweaver.net>
2013-08-28 15:39:40 +0200

    call task graph file utils to export a empty graph in main

--- src/com/github/noxan/blommagraphs/Main.java
+++ src/com/github/noxan/blommagraphs/Main.java
@@ -1,7 +1,23 @@
 package com.github.noxan.blommagraphs;
 
 
+import java.io.IOException;
+
+import com.github.noxan.blommagraphs.graphs.DefaultTaskGraph;
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.serializer.STGSerializer;
+import com.github.noxan.blommagraphs.utils.TaskGraphFileUtils;
+
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("blomma-graphs");
+
+        TaskGraph graph = new DefaultTaskGraph();
+        try {
+            TaskGraphFileUtils.writeFile("graph.stg", graph, new STGSerializer());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
