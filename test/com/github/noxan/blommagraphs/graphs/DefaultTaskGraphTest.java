dcdc7ebfc40531780bfeb5e682331a005029b83d
noxan <noxan@byteweaver.net>
2013-08-29 11:24:48 +0200

    create empty test case for defaulttaskgraph in new test directory

---
+++ test/com/github/noxan/blommagraphs/graphs/DefaultTaskGraphTest.java
@@ -0,0 +1,14 @@
+package com.github.noxan.blommagraphs.graphs;
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+public class DefaultTaskGraphTest {
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
