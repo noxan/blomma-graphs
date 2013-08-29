75432c277a0fb0661199f14fc8534d6ac3aea854
ruction <benjaminwoehrl@googlemail.com>
2013-08-29 18:25:49 +0200

    created generator exceptions

---
+++ src/com/github/noxan/blommagraphs/generator/exceptions/OutOfRangeException.java
@@ -0,0 +1,7 @@
+package com.github.noxan.blommagraphs.generator.exceptions;
+
+public class OutOfRangeException extends GeneratorException {
+    public OutOfRangeException() {
+        super("Wrong input!! Out of range...");
+    }
+}
