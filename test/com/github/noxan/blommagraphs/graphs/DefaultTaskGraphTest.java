03da5b89c53be33c52f932e41e7c22de5a03186c
noxan <noxan@byteweaver.net>
2013-08-29 11:30:38 +0200

    add run with annotation to dummy test case

--- test/com/github/noxan/blommagraphs/graphs/DefaultTaskGraphTest.java
+++ test/com/github/noxan/blommagraphs/graphs/DefaultTaskGraphTest.java
@@ -4,8 +4,11 @@ package com.github.noxan.blommagraphs.graphs;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 
+@RunWith(JUnit4.class)
 public class DefaultTaskGraphTest {
     @Test
     public void test() {
