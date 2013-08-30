c50f5f491a857f357b2e2f6a2bd6f25279a80abb
noxan <noxan@byteweaver.net>
2013-08-30 13:58:51 +0200

    add a small set of first test cases for jgrapht task graph impl

---
+++ test/com/github/noxan/blommagraphs/graphs/impl/JGraphtTaskGraphTest.java
@@ -0,0 +1,42 @@
+package com.github.noxan.blommagraphs.graphs.impl;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.exceptions.DuplicateEdgeException;
+
+
+@RunWith(JUnit4.class)
+public class JGraphtTaskGraphTest {
+    private TaskGraph taskGraph;
+
+    @Before
+    public void initialize() {
+        taskGraph = new JGraphtTaskGraph();
+    }
+
+    @Test
+    public void testInsertNode() {
+        taskGraph.insertNode(taskGraph.getFirstNode(), 1, taskGraph.getLastNode(), 1, 1);
+    }
+
+    @Test(expected = DuplicateEdgeException.class)
+    public void testInsertDuplicateEdge() throws DuplicateEdgeException {
+        taskGraph.insertEdge(taskGraph.getFirstNode(), taskGraph.getLastNode(), 2);
+    }
+
+    @Test
+    public void testGetNodeSet() {
+        Assert.assertEquals(taskGraph.getNodeSet().size(), 2);
+    }
+
+    @Test
+    public void testGetEdgeSet() {
+        Assert.assertEquals(taskGraph.getEdgeSet().size(), 1);
+    }
+}
