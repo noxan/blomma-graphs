e3c837363e0dec5be11a9d4733a7b13c2c78d585
noxan <noxan@byteweaver.net>
2013-08-30 12:29:08 +0200

    create class for jgrapht task graph node impl

---
+++ src/com/github/noxan/blommagraphs/graphs/impl/JGraphtTaskGraphNode.java
@@ -0,0 +1,69 @@
+package com.github.noxan.blommagraphs.graphs.impl;
+
+
+import java.util.List;
+
+import com.github.noxan.blommagraphs.graphs.TaskGraphEdge;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+
+
+public class JGraphtTaskGraphNode implements TaskGraphNode {
+    public JGraphtTaskGraphNode() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public List<TaskGraphNode> getPrevNodes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<TaskGraphNode> getNextNodes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getPrevNodeCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNextNodeCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<TaskGraphEdge> getPrevEdges() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<TaskGraphEdge> getNextEdges() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getPrevEdgeCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNextEdgeCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getComputationTime() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
