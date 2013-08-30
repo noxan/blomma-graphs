b2b64421749b5ef238c6c25ebbc9384761213da8
noxan <noxan@byteweaver.net>
2013-08-30 13:14:29 +0200

    create class for jgrapht implementation of task graph edge

---
+++ src/com/github/noxan/blommagraphs/graphs/impl/JGraphtTaskGraphEdge.java
@@ -0,0 +1,27 @@
+package com.github.noxan.blommagraphs.graphs.impl;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraphEdge;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+
+
+public class JGraphtTaskGraphEdge implements TaskGraphEdge {
+    @Override
+    public TaskGraphNode getPrevNode() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TaskGraphNode getNextNode() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getCommunicationTime() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
