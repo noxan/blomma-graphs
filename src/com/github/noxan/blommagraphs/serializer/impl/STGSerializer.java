4d9857eb5c24e518deac63b08a272742952a533d
namelessvoid <pommesdiefritte@gmx.de>
2013-08-30 11:08:15 +0200

    Added STGSerializer.serialize().

--- src/com/github/noxan/blommagraphs/serializer/impl/STGSerializer.java
+++ src/com/github/noxan/blommagraphs/serializer/impl/STGSerializer.java
@@ -2,6 +2,8 @@ package com.github.noxan.blommagraphs.serializer.impl;
 
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.TaskGraphEdge;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
 import com.github.noxan.blommagraphs.graphs.impl.DefaultTaskGraph;
 import com.github.noxan.blommagraphs.serializer.TaskGraphSerializer;
 
@@ -9,8 +11,20 @@ import com.github.noxan.blommagraphs.serializer.TaskGraphSerializer;
 public class STGSerializer implements TaskGraphSerializer {
     @Override
     public String serialize(TaskGraph graph) {
-        // TODO Auto-generated method stub
-        return "";
+        StringBuffer stringBuffer = new StringBuffer();
+
+        // Iterate through nodes
+        for (TaskGraphNode node : graph.getNodeSet()) {
+            stringBuffer.append(String.format("(%i) (%i) (%i)\n", node.getID(),
+                    node.getComputationTime(), node.getPrevNodeCount()));
+
+            // Iterate through dependencies of node
+            for (TaskGraphEdge edge : node.getPrevEdges()) {
+                String.format("\t(%i) (%i)\n", edge.getPrevNode().getID(),
+                        edge.getCommunicationTime());
+            }
+        }
+        return stringBuffer.toString();
     }
 
     @Override
