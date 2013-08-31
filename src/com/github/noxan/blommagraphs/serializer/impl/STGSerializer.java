2636ee8281d82d0135e08741e55d1d5418e35149
namelessvoid <pommesdiefritte@gmx.de>
2013-09-01 00:37:00 +0200

    Added some meta infos.

--- src/com/github/noxan/blommagraphs/serializer/impl/STGSerializer.java
+++ src/com/github/noxan/blommagraphs/serializer/impl/STGSerializer.java
@@ -32,6 +32,7 @@ public class STGSerializer implements TaskGraphSerializer {
                         edge.getCommunicationTime()));
             }
         }
+        stringBuffer.append("\n").append(generateMetaInfo(graph));
         return stringBuffer.toString();
     }
 
@@ -39,5 +40,20 @@ public class STGSerializer implements TaskGraphSerializer {
     public TaskGraph deserialize(String graphString) {
         // TODO Auto-generated method stub
         return new DefaultTaskGraph();
+    }
+
+    private String generateMetaInfo(TaskGraph graph) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("# BlommaGraphs:\tthis is a Standard Task Graph project file\n");
+        // Nodes
+        stringBuffer
+                .append(String.format("# Tasks:\t%d (+ dummy tasks: 2)\n", graph.getNodeCount()));
+        // Layers
+        stringBuffer.append(String.format("# Layers:\t%d\n", graph.getLayerCount()));
+        // Edges
+        stringBuffer.append(String.format("# Edges:\t%d (+ dummy edges: %d)\n",
+                graph.getEdgeCount(), graph.getFirstNode().getNextEdgeCount()
+                        + graph.getLastNode().getPrevEdgeCount()));
+        return stringBuffer.toString();
     }
 }
