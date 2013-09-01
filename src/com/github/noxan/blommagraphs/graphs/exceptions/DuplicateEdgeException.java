779bf39bf5b1b30413b46d5d6914a42132d28d1a
noxan <noxan@byteweaver.net>
2013-09-01 11:13:36 +0200

    add documentation to duplicate edge exception

--- src/com/github/noxan/blommagraphs/graphs/exceptions/DuplicateEdgeException.java
+++ src/com/github/noxan/blommagraphs/graphs/exceptions/DuplicateEdgeException.java
@@ -1,6 +1,12 @@
 package com.github.noxan.blommagraphs.graphs.exceptions;
 
 
+/**
+ * An exception to be used when an edge should be inserted but there is already
+ * an existing one between the two nodes.
+ * 
+ * @author noxan
+ */
 public class DuplicateEdgeException extends Exception {
     private static final long serialVersionUID = 3975611637043020044L;
 }
