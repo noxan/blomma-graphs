efd9d76b37a01307c64157081f1ff91274ad883c
namelessvoid <pommesdiefritte@gmx.de>
2013-09-01 15:43:29 +0200

    Added some javadoc to TaskGraphSerializer.

--- src/com/github/noxan/blommagraphs/serializer/TaskGraphSerializer.java
+++ src/com/github/noxan/blommagraphs/serializer/TaskGraphSerializer.java
@@ -5,7 +5,19 @@ import com.github.noxan.blommagraphs.graphs.TaskGraph;
 
 
 public interface TaskGraphSerializer {
+    /**
+     * Serialize a graph into a string.
+     * 
+     * @param graph TaskGraph that is serialized.
+     * @return String representation of serialized graph.
+     */
     public String serialize(TaskGraph graph);
 
+    /**
+     * Deserialize a graph into a string.
+     * 
+     * @param graphString String thas is deserialized.
+     * @return TaskGraph representation of deserialized graphString.
+     */
     public TaskGraph deserialize(String graphString);
 }
