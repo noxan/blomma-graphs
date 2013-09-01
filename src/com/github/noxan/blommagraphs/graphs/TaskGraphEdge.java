cd5ffa3994bdfd26088c0bc2b3b46834f407a604
noxan <noxan@byteweaver.net>
2013-09-01 11:07:31 +0200

    add documentation for the task graph edge interface

--- src/com/github/noxan/blommagraphs/graphs/TaskGraphEdge.java
+++ src/com/github/noxan/blommagraphs/graphs/TaskGraphEdge.java
@@ -1,10 +1,30 @@
 package com.github.noxan.blommagraphs.graphs;
 
 
+/**
+ * Generic interface for a task graph edge.
+ * 
+ * @author noxan
+ */
 public interface TaskGraphEdge {
+    /**
+     * Returns the node where the edges starts.
+     * 
+     * @return previous node
+     */
     public TaskGraphNode getPrevNode();
 
+    /**
+     * Returns the node to which the edges goes.
+     * 
+     * @return next node
+     */
     public TaskGraphNode getNextNode();
 
+    /**
+     * Returns the communication time (weight) of this edge.
+     * 
+     * @return communication time
+     */
     public int getCommunicationTime();
 }
