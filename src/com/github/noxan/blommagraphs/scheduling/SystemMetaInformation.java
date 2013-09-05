fb572246b55cbec387d48d34038babfd8c9d3ea9
noxan <noxan@byteweaver.net>
2013-09-05 10:45:30 +0200

    add getter and setter for processor count in system meta information

--- src/com/github/noxan/blommagraphs/scheduling/SystemMetaInformation.java
+++ src/com/github/noxan/blommagraphs/scheduling/SystemMetaInformation.java
@@ -2,5 +2,7 @@ package com.github.noxan.blommagraphs.scheduling;
 
 
 public interface SystemMetaInformation {
+    public void setProcessorCount(int processorCount);
 
+    public int getProcessorCount();
 }
