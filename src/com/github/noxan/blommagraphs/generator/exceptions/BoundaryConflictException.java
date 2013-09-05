0de275b8dd3ddfce10698707514257c87695d8cf
noxan <noxan@byteweaver.net>
2013-09-05 16:14:48 +0200

    add serialVersionUID to generator exceptions

--- src/com/github/noxan/blommagraphs/generator/exceptions/BoundaryConflictException.java
+++ src/com/github/noxan/blommagraphs/generator/exceptions/BoundaryConflictException.java
@@ -5,6 +5,8 @@ package com.github.noxan.blommagraphs.generator.exceptions;
  * @author Freax
  */
 public class BoundaryConflictException extends GeneratorException {
+    private static final long serialVersionUID = 8554208185844382115L;
+
     public BoundaryConflictException() {
         super("Boundary conflict! Wrong Boundary...");
     }
