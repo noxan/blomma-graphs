df895ea29b84a16f2a8e89ae6665af5497797e63
noxan <noxan@byteweaver.net>
2013-09-05 13:49:08 +0200

    add test cases for get first and last node and get edge set in default taskgraph impl

--- test/com/github/noxan/blommagraphs/graphs/impl/DefaultTaskGraphTest.java
+++ test/com/github/noxan/blommagraphs/graphs/impl/DefaultTaskGraphTest.java
@@ -1,17 +1,45 @@
 package com.github.noxan.blommagraphs.graphs.impl;
 
 
-import static org.junit.Assert.fail;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+
 
 @RunWith(JUnit4.class)
 public class DefaultTaskGraphTest {
+    private TaskGraph taskGraph;
+
+    @Before
+    public void initialize() {
+        taskGraph = new DefaultTaskGraph();
+    }
+
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testGetFirstNode() {
+        if (taskGraph.getFirstNode() == null) {
+            Assert.fail("First node is null.");
+        }
+        Assert.assertTrue(taskGraph.getFirstNode() instanceof TaskGraphNode);
+        Assert.assertTrue(taskGraph.getFirstNode() instanceof DefaultTaskGraphNode);
+    }
+
+    @Test
+    public void testGetLastNode() {
+        if (taskGraph.getLastNode() == null) {
+            Assert.fail("Last node is null.");
+        }
+        Assert.assertTrue(taskGraph.getLastNode() instanceof TaskGraphNode);
+        Assert.assertTrue(taskGraph.getLastNode() instanceof DefaultTaskGraphNode);
+    }
+
+    @Test
+    public void testGetEdgeSetWithEmptyGraph() {
+        Assert.assertEquals(taskGraph.getEdgeSet().size(), 1);
     }
 }
