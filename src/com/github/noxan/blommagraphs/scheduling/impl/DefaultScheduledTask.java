c3f09c7b491290a7e0ea3792c9eb19844cc36a2b
namelessvoid <pommesdiefritte@gmx.de>
2013-09-05 11:10:05 +0200

    Implemented DefaultScheduledTask.

---
+++ src/com/github/noxan/blommagraphs/scheduling/impl/DefaultScheduledTask.java
@@ -0,0 +1,54 @@
+package com.github.noxan.blommagraphs.scheduling.impl;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
+
+
+public class DefaultScheduledTask implements ScheduledTask {
+    private int startTime;
+    private int cpuId;
+    private int communicationTime;
+    private TaskGraphNode node;
+
+    @Override
+    public int getStartTime() {
+        return startTime;
+    }
+
+    @Override
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    @Override
+    public int getCpuId() {
+        return cpuId;
+    }
+
+    @Override
+    public void setCpuId(int cpuId) {
+        this.cpuId = cpuId;
+    }
+
+    @Override
+    public int getCommunicationTime() {
+        return communicationTime;
+    }
+
+    @Override
+    public void setCommunicationTime(int communicationTime) {
+        this.communicationTime = communicationTime;
+    }
+
+    @Override
+    public TaskGraphNode getTaskGraphNode() {
+        return node;
+    }
+
+    @Override
+    public void setTaskGraphNode(TaskGraphNode node) {
+        this.node = node;
+    }
+
+}
