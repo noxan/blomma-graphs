d9cb9edb92aff2be3a48132251c4c4c0e4c0bec0
namelessvoid <pommesdiefritte@gmx.de>
2013-09-05 11:04:31 +0200

    Filled ScheduledTask interface with methods.

--- src/com/github/noxan/blommagraphs/scheduling/ScheduledTask.java
+++ src/com/github/noxan/blommagraphs/scheduling/ScheduledTask.java
@@ -1,5 +1,24 @@
 package com.github.noxan.blommagraphs.scheduling;
 
+
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+
+
 public interface ScheduledTask {
 
+    public void setStartTime(int startTime);
+
+    public int getStartTime();
+
+    public void setCpuId(int cpuId);
+
+    public int getCpuId();
+
+    public void setCommunicationTime(int communicationTime);
+
+    public int getCommunicationTime();
+
+    public void setTaskGraphNode(TaskGraphNode node);
+
+    public TaskGraphNode getTaskGraphNode();
 }
