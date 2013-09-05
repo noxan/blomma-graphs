eb130db0c7fd9d0a699b00f7c865b4cf49cd3b0d
namelessvoid <pommesdiefritte@gmx.de>
2013-09-05 11:15:51 +0200

    Created scheduling.serializer package and added ScheduledTaskListSerializer class.

---
+++ src/com/github/noxan/blommagraphs/scheduling/serializer/ScheduledTaskListSerializer.java
@@ -0,0 +1,18 @@
+package com.github.noxan.blommagraphs.scheduling.serializer;
+
+
+import java.util.List;
+
+import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
+
+
+public interface ScheduledTaskListSerializer {
+    /**
+     * Serializes a list of ScheduledTasks.
+     * 
+     * @param scheduledTaskList List of ScheduledTasks ordered by their starting
+     *            time.
+     * @return String representation of the ScheduledTask List.
+     */
+    public String serialize(List<ScheduledTask> scheduledTaskList);
+}
