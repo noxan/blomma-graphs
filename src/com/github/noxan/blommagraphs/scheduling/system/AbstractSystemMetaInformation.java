34173be1b50cabb96fadd54980a29246d12bf366
noxan <noxan@byteweaver.net>
2013-09-05 10:52:32 +0200

    implement abstract system meta information class

--- src/com/github/noxan/blommagraphs/scheduling/system/AbstractSystemMetaInformation.java
+++ src/com/github/noxan/blommagraphs/scheduling/system/AbstractSystemMetaInformation.java
@@ -1,6 +1,24 @@
 package com.github.noxan.blommagraphs.scheduling.system;
 
 
-public abstract class AbstractSystemMetaInformation {
+public abstract class AbstractSystemMetaInformation implements SystemMetaInformation {
+    private int processorCount;
 
+    public AbstractSystemMetaInformation() {
+        this(2);
+    }
+
+    public AbstractSystemMetaInformation(int processorCount) {
+        setProcessorCount(processorCount);
+    }
+
+    @Override
+    public int getProcessorCount() {
+        return processorCount;
+    }
+
+    @Override
+    public void setProcessorCount(int processorCount) {
+        this.processorCount = processorCount;
+    }
 }
