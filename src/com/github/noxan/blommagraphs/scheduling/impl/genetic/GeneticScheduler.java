6721de8ff440a8ef4c4c5b9d51f1f7c7da1aab25
noxan <noxan@byteweaver.net>
2013-09-06 14:14:25 +0200

    create genetic scheduler class

---
+++ src/com/github/noxan/blommagraphs/scheduling/impl/genetic/GeneticScheduler.java
@@ -0,0 +1,17 @@
+package com.github.noxan.blommagraphs.scheduling.impl.genetic;
+
+
+import java.util.List;
+
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
+import com.github.noxan.blommagraphs.scheduling.Scheduler;
+
+
+public class GeneticScheduler implements Scheduler {
+    @Override
+    public List<ScheduledTask> schedule(TaskGraph graph) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
