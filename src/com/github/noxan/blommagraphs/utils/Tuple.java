ca722ae0a873c81e88dedf20689b414c779d5b40
noxan <noxan@byteweaver.net>
2013-09-06 14:03:50 +0200

    implement generic tuple class

--- src/com/github/noxan/blommagraphs/utils/Tuple.java
+++ src/com/github/noxan/blommagraphs/utils/Tuple.java
@@ -1,5 +1,31 @@
 package com.github.noxan.blommagraphs.utils;
 
-public class Tuple {
 
+public class Tuple<A, B> {
+    private A a;
+    private B b;
+
+    public Tuple() {
+    }
+
+    public Tuple(A a, B b) {
+        setFirst(a);
+        setSecond(b);
+    }
+
+    public A getFirst() {
+        return a;
+    }
+
+    public B getSecond() {
+        return b;
+    }
+
+    public void setFirst(A a) {
+        this.a = a;
+    }
+
+    public void setSecond(B b) {
+        this.b = b;
+    }
 }
