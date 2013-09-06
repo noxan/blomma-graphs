9425b3fb1de5a6d9cf43235fbb06f9a714a3188d
LaPush <frostbeule10000@msn.com>
2013-09-06 17:24:54 +0200

    set up DynamicLevelSchedulerImpl with dummy functions

--- src/com/github/noxan/blommagraphs/scheduling/impl/dls/DynamicLevelSchedulerImpl.java
+++ src/com/github/noxan/blommagraphs/scheduling/impl/dls/DynamicLevelSchedulerImpl.java
@@ -1,6 +1,8 @@
+
 import java.util.List;
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
 import com.github.noxan.blommagraphs.scheduling.Scheduler;
 import com.github.noxan.blommagraphs.scheduling.system.SystemMetaInformation;
@@ -13,7 +15,17 @@ public class DynamicLevelSchedulerImpl implements Scheduler {
     public List<ScheduledTask> schedule(TaskGraph graph, SystemMetaInformation systemInformation) {
         // compute static level of all nodes
         // initialize ready-pool at first only start node
+
+        // compute the earliest start time for every ready node for each
+        // processor LaPush
+        // choose the node-processor pair with the biggest dynamic level and
+        // comit it to the processor
+        // add new ready nodes to the ready node pool
         return null;
+    }
+
+    public boolean isReadyNode(TaskGraphNode taskGraphNode) {
+        return true;
     }
 
 }
