f6b1da821f497af984a89a49f97b49435d5c9bc2
namelessvoid <pommesdiefritte@gmx.de>
2013-09-06 14:38:11 +0200

    Created LASTNode helper class.

---
+++ src/com/github/noxan/blommagraphs/scheduling/impl/last/LASTNode.java
@@ -0,0 +1,57 @@
+package com.github.noxan.blommagraphs.scheduling.impl.last;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+
+
+public class LASTNode {
+    private TaskGraphNode taskGraphNode;
+    private float dNode;
+    private int dEdge;
+    private float strength;
+    private Status status;
+
+    public LASTNode(TaskGraphNode taskGraphNode) {
+        this.taskGraphNode = taskGraphNode;
+    }
+
+    public TaskGraphNode getTaskGraphNode() {
+        return taskGraphNode;
+    }
+
+    public float getdNode() {
+        return dNode;
+    }
+
+    public void setdNode(float dNode) {
+        this.dNode = dNode;
+    }
+
+    public int getdEdge() {
+        return dEdge;
+    }
+
+    public void setdEdge(int dEdge) {
+        this.dEdge = dEdge;
+    }
+
+    public float getStrength() {
+        return strength;
+    }
+
+    public void setStrength(float strength) {
+        this.strength = strength;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public enum Status {
+        FRONTIER, SCHEDULED
+    };
+}
