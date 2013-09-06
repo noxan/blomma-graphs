94177bf91f60560226ef7a52a5fc8f6133c11e63
namelessvoid <pommesdiefritte@gmx.de>
2013-09-06 17:09:35 +0200

    Removed useless attributes and methods from LASTNode.

--- src/com/github/noxan/blommagraphs/scheduling/impl/last/LASTNode.java
+++ src/com/github/noxan/blommagraphs/scheduling/impl/last/LASTNode.java
@@ -11,8 +11,6 @@ public class LASTNode {
 
     private TaskGraphNode taskGraphNode;
     private float dNode;
-    private int dEdge;
-    private float strength;
     private Status status;
     private int cpuId;
 
@@ -30,22 +28,6 @@ public class LASTNode {
 
     public void setdNode(float dNode) {
         this.dNode = dNode;
-    }
-
-    public int getdEdge() {
-        return dEdge;
-    }
-
-    public void setdEdge(int dEdge) {
-        this.dEdge = dEdge;
-    }
-
-    public float getStrength() {
-        return strength;
-    }
-
-    public void setStrength(float strength) {
-        this.strength = strength;
     }
 
     public Status getStatus() {
