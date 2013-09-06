11fd57c27bfea2f26ef8ad6c32bde1f4baa66f9d
noxan <noxan@byteweaver.net>
2013-09-07 00:42:06 +0200

    remove old not working chromosome decoding stuff

--- src/com/github/noxan/blommagraphs/scheduling/impl/genetic/utils/Chromosome.java
+++ src/com/github/noxan/blommagraphs/scheduling/impl/genetic/utils/Chromosome.java
@@ -8,10 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
-import com.github.noxan.blommagraphs.graphs.TaskGraphEdge;
 import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
-import com.github.noxan.blommagraphs.graphs.exceptions.ContainsNoEdgeException;
-import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
 import com.github.noxan.blommagraphs.scheduling.impl.DefaultScheduledTask;
 import com.github.noxan.blommagraphs.scheduling.impl.DefaultScheduledTaskList;
@@ -93,46 +90,6 @@ public class Chromosome {
         decode(taskGraph.getFirstNode(), taskGraph, scheduledTaskList, unscheduledNodes);
 
         // TODO: maybe add a readyNode list to select from?!
-
-        Iterator<TaskGraphNode> it = unscheduledNodes.iterator();
-        while (it.hasNext()) {
-            TaskGraphNode taskNode = it.next();
-
-            // processorId for the task
-            int processorId = getProcessorForTask(taskNode);
-
-            // search for a previous scheduled task on this processor
-            ScheduledTask lastScheduledTask = scheduledTaskList
-                    .getLastScheduledTaskOnProcessor(processorId);
-
-            try {
-                TaskGraphEdge prevEdge;
-                if (lastScheduledTask == null) {
-                    prevEdge = taskNode.getPrevEdges().iterator().next();
-                } else {
-                    prevEdge = taskGraph.findEdge(lastScheduledTask.getTaskGraphNode(), taskNode);
-                }
-
-                int communicationTime = prevEdge.getCommunicationTime();
-                // set communicationTime to zero if previous task is on same processor
-                if (lastScheduledTask != null
-                        && scheduledTaskList.isTaskOnProcessor(processorId,
-                                lastScheduledTask.getTaskId())) {
-                    communicationTime = 0;
-                }
-
-                int startTime = communicationTime;
-                if (lastScheduledTask != null) {
-                    startTime += lastScheduledTask.getFinishTime();
-                }
-
-                ScheduledTask scheduledTask = new DefaultScheduledTask(startTime, processorId,
-                        communicationTime, taskNode);
-                scheduledTaskList.add(scheduledTask);
-            } catch (ContainsNoEdgeException e) {
-                e.printStackTrace();
-            }
-        }
 
         return scheduledTaskList;
     }
