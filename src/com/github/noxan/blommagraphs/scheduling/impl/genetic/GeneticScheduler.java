d4c5a349da1303d9c33480deeccd33ec8571955a
noxan <noxan@byteweaver.net>
2013-09-06 15:05:33 +0200

    add constrcutor for initial population to gs and implement initialize method
    to setup sorted lists for all tasks grouped by cpu

--- src/com/github/noxan/blommagraphs/scheduling/impl/genetic/GeneticScheduler.java
+++ src/com/github/noxan/blommagraphs/scheduling/impl/genetic/GeneticScheduler.java
@@ -1,7 +1,10 @@
 package com.github.noxan.blommagraphs.scheduling.impl.genetic;
 
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
@@ -20,10 +23,32 @@ public class GeneticScheduler implements Scheduler {
         this.initialScheduler = initialScheduler;
     }
 
+    public GeneticScheduler(List<ScheduledTask> initialPopulation) {
+        this.initialPopulation = initialPopulation;
+    }
+
+    private void initialize() {
+        if (initialPopulation == null) {
+            initialPopulation = initialScheduler.schedule(metaInformation, taskGraph);
+        }
+
+        // group scheduled tasks by CPU
+        Map<Integer, List<ScheduledTask>> scheduledTasksGroupedByCPU = new HashMap<Integer, List<ScheduledTask>>();
+        for (ScheduledTask task : initialPopulation) {
+            scheduledTasksGroupedByCPU.get(task.getCpuId()).add(task);
+        }
+        // sort lists by startTime
+        for (List<ScheduledTask> scheduledTasks : scheduledTasksGroupedByCPU.values()) {
+            Collections.sort(scheduledTasks);
+        }
+    }
+
     @Override
     public List<ScheduledTask> schedule(SystemMetaInformation metaInformation, TaskGraph taskGraph) {
         this.metaInformation = metaInformation;
         this.taskGraph = taskGraph;
+
+        initialize();
 
         // TODO Auto-generated method stub
         return null;
