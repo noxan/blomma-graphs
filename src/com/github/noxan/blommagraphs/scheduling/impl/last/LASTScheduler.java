1fce47b1558541a06c3ebbfc7a2ed188a00da5f1
ruction <benjaminwoehrl@googlemail.com>
2013-09-06 16:43:19 +0200

    implemented calcStrength in LASTscheduler

--- src/com/github/noxan/blommagraphs/scheduling/impl/last/LASTScheduler.java
+++ src/com/github/noxan/blommagraphs/scheduling/impl/last/LASTScheduler.java
@@ -1,15 +1,16 @@
 package com.github.noxan.blommagraphs.scheduling.impl.last;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
 import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+import com.github.noxan.blommagraphs.graphs.exceptions.ContainsNoEdgeException;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
 import com.github.noxan.blommagraphs.scheduling.Scheduler;
 import com.github.noxan.blommagraphs.scheduling.system.SystemMetaInformation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 
 public class LASTScheduler implements Scheduler {
@@ -102,7 +103,44 @@ public class LASTScheduler implements Scheduler {
      * @return STRENGTH of node1 related to cpu.
      */
     private float calcStrength(LASTNode node1, int cpuId) {
-        return 0.0f;
+        //
+        int computationTimeNode1 = node1.getTaskGraphNode().getComputationTime();
+        float strength = 0f;
+
+        //
+        ArrayList<TaskGraphNode> lastNodePrevNodes = new ArrayList<TaskGraphNode>
+                (node1.getTaskGraphNode().getPrevNodes());
+        ArrayList<TaskGraphNode> lastNodeNextNodes = new ArrayList<TaskGraphNode>
+                (node1.getTaskGraphNode().getNextNodes());
+
+        //
+        for (int i = 0; i < lastNodePrevNodes.size(); i++) {
+            try {
+                strength += ((taskGraph.findEdge(lastNodePrevNodes.get(i),
+                        node1.getTaskGraphNode()).getCommunicationTime()) / computationTimeNode1);
+            } catch (ContainsNoEdgeException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < lastNodeNextNodes.size(); i++) {
+            try{
+                strength += ((taskGraph.findEdge(node1.getTaskGraphNode(),
+                        lastNodeNextNodes.get(i)).getCommunicationTime()) / computationTimeNode1);
+            } catch (ContainsNoEdgeException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < lastNodeNextNodes.size(); i++) {
+            try {
+                strength -= ( computationTimeNode1 / (taskGraph.findEdge(node1.getTaskGraphNode(),
+                        lastNodeNextNodes.get(i)).getCommunicationTime()));
+            } catch (ContainsNoEdgeException e) {
+                e.printStackTrace();
+            }
+        }
+        return strength;
     }
 
 }
