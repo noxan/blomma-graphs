b9c5b68e142a0e74210eca34182886807aeaf924
namelessvoid <pommesdiefritte@gmx.de>
2013-09-07 12:23:59 +0200

    Fixed wrong node assignment.

--- test/com/github/noxan/blommagraphs/scheduling/impl/last/LASTSchedulerTest.java
+++ test/com/github/noxan/blommagraphs/scheduling/impl/last/LASTSchedulerTest.java
@@ -43,7 +43,7 @@ public class LASTSchedulerTest {
 
         graphNodes[1] = graph.insertNode(graph.getFirstNode(), 5, graph.getLastNode(), 5, 20);
         graphNodes[2] = graph.insertNode(graph.getFirstNode(), 5, graph.getLastNode(), 50, 30);
-        graphNodes[4] = graph.insertNode(graphNodes[2], 10, graph.getLastNode(), 5, 40);
+        graphNodes[3] = graph.insertNode(graphNodes[2], 10, graph.getLastNode(), 5, 40);
 
         lastScheduler.initialize(graph, systemInformation);
     }
