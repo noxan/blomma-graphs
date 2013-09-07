f644911f032c5cfb552d9d10df6e954ac3a53d3e
namelessvoid <pommesdiefritte@gmx.de>
2013-09-07 10:26:52 +0200

    Fixed deprecated reference.

--- test/com/github/noxan/blommagraphs/scheduling/impl/last/LASTSchedulerTest.java
+++ test/com/github/noxan/blommagraphs/scheduling/impl/last/LASTSchedulerTest.java
@@ -37,7 +37,7 @@ public class LASTSchedulerTest extends LASTScheduler {
 
         graphNodes[1] = graph.insertNode(graph.getFirstNode(), 5, graph.getLastNode(), 5, 20);
         graphNodes[2] = graph.insertNode(graph.getFirstNode(), 5, graph.getLastNode(), 50, 30);
-        graphNodes[4] = graph.insertNode(node3, 10, graph.getLastNode(), 5, 40);
+        graphNodes[4] = graph.insertNode(graphNodes[2], 10, graph.getLastNode(), 5, 40);
     }
 
     @Test
