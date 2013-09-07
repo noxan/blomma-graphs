b8ca3482540b385027052ae664ca7411b6576f1a
namelessvoid <pommesdiefritte@gmx.de>
2013-09-07 10:19:59 +0200

    Built test graph for LASTSchedulerTest.

--- test/com/github/noxan/blommagraphs/scheduling/impl/last/LASTSchedulerTest.java
+++ test/com/github/noxan/blommagraphs/scheduling/impl/last/LASTSchedulerTest.java
@@ -8,17 +8,31 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
 import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
 import com.github.noxan.blommagraphs.graphs.impl.DefaultTaskGraphNode;
+import com.github.noxan.blommagraphs.graphs.impl.JGraphtTaskGraph;
 
 
 @RunWith(JUnit4.class)
 public class LASTSchedulerTest extends LASTScheduler {
-    private LASTScheduler lastScheduler;
+    // private LASTScheduler lastScheduler;
+    private TaskGraph graph;
 
     @Before
     public void initialize() {
-        lastScheduler = new LASTScheduler();
+        // lastScheduler = new LASTScheduler();
+
+        // Building up an example graph statet in
+        // http://www.eng.auburn.edu/files/acad_depts/csse/csse_technical_reports/CSSE91-14.pdf
+        // on page 8.
+        graph = new JGraphtTaskGraph();
+        graph.getFirstNode().setComputationTime(10);
+        graph.getLastNode().setComputationTime(50);
+        graph.insertNode(graph.getFirstNode(), 5, graph.getLastNode(), 5, 20);
+        TaskGraphNode node3 = graph
+                .insertNode(graph.getFirstNode(), 5, graph.getLastNode(), 50, 30);
+        graph.insertNode(node3, 10, graph.getLastNode(), 5, 40);
     }
 
     @Test
