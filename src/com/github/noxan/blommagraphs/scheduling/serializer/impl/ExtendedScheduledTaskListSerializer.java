736237d49225edc54dfa668f3b672944d45d69a8
noxan <noxan@byteweaver.net>
2013-09-07 13:48:15 +0200

    implement extended scheduled task list serializer with more debug information

---
+++ src/com/github/noxan/blommagraphs/scheduling/serializer/impl/ExtendedScheduledTaskListSerializer.java
@@ -0,0 +1,24 @@
+package com.github.noxan.blommagraphs.scheduling.serializer.impl;
+
+
+import java.util.List;
+
+import com.github.noxan.blommagraphs.scheduling.ScheduledTask;
+import com.github.noxan.blommagraphs.scheduling.serializer.ScheduledTaskListSerializer;
+
+
+public class ExtendedScheduledTaskListSerializer implements ScheduledTaskListSerializer {
+    @Override
+    public String serialize(List<ScheduledTask> scheduledTaskList) {
+        StringBuffer string = new StringBuffer();
+
+        string.append("sT\tp\ttask\tcompT\tcommT\n\n");
+        for (ScheduledTask task : scheduledTaskList) {
+            string.append(String.format("%d\t%d\t%d\t%d\t%d\n", task.getStartTime(),
+                    task.getCpuId(), task.getTaskId(), task.getComputationTime(),
+                    task.getCommunicationTime()));
+        }
+        // Add meta info here, if necessary.
+        return string.toString();
+    }
+}
