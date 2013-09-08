0f819a578fe476db86b017576887d86d9e5f4a48
namelessvoid <pommesdiefritte@gmx.de>
2013-09-08 22:46:53 +0200

    TaskGraphFileUtils now uses FileUtils helper class.

--- src/com/github/noxan/blommagraphs/utils/TaskGraphFileUtils.java
+++ src/com/github/noxan/blommagraphs/utils/TaskGraphFileUtils.java
@@ -1,14 +1,7 @@
 package com.github.noxan.blommagraphs.utils;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
 import com.github.noxan.blommagraphs.serializer.TaskGraphSerializer;
@@ -30,15 +23,7 @@ public class TaskGraphFileUtils {
      */
     public static void writeFile(String pathname, TaskGraph graph, TaskGraphSerializer serializer)
             throws IOException {
-        FileOutputStream fos = new FileOutputStream(new File(pathname));
-
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
-        String graphString = serializer.serialize(graph);
-
-        writer.write(graphString);
-
-        writer.close();
-        fos.close();
+        FileUtils.writeFile(pathname, serializer.serialize(graph));
     }
 
     /**
@@ -51,18 +36,7 @@ public class TaskGraphFileUtils {
      */
     public static TaskGraph readFile(String pathname, TaskGraphSerializer deserializer)
             throws IOException {
-        FileInputStream fis = new FileInputStream(new File(pathname));
-
-        StringBuilder graphString = new StringBuilder();
-        String line;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-        while ((line = reader.readLine()) != null) {
-            graphString.append(line + "\n");
-        }
-
-        reader.close();
-        fis.close();
-
+        String graphString = FileUtils.readFile(pathname);
         return deserializer.deserialize(graphString.toString());
     }
 }
