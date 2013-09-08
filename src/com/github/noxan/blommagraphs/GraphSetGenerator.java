7df91967b981511373098d8d8b0470791f3e27de
namelessvoid <pommesdiefritte@gmx.de>
2013-09-08 22:07:42 +0200

    Moved Main.java to GraphSetGenerator.java. Implemented generator loop to generate all required kinds of graphs.

--- src/com/github/noxan/blommagraphs/GraphSetGenerator.java
+++ src/com/github/noxan/blommagraphs/GraphSetGenerator.java
@@ -14,15 +14,21 @@ import com.github.noxan.blommagraphs.serializer.impl.STGSerializer;
 import com.github.noxan.blommagraphs.utils.TaskGraphFileUtils;
 
 
-public class Main {
+/**
+ * Generates a set of graphs.
+ * 
+ * Sizes: 10, 50, 100, 300, 500<br />
+ * Number of graphs per size: 100
+ * 
+ */
+public class GraphSetGenerator {
     public static void main(String[] args) {
-        System.out.println("blomma-graphs");
-
         // settings
-        String folderName = "export/";
-        int numberOfGraphs = 10;
+        String rootFolder = "export/";
+        String graphsFolder = "";
+        int numberOfGraphs = 100;
         // graph generator settings
-        int numberOfNodes = 10;
+        int numberOfNodes[] = { 10, 50, 100, 300, 500 };
         int minIncomgEdges = 1;
         int maxIncomingEdges = 2;
         int spreadEdges = 1;
@@ -34,14 +40,11 @@ public class Main {
         int spreadCommunicationTime = 1;
 
         // logging
-        System.out.println(String.format("Writing exports to folder: %s", folderName));
-        new File(folderName).mkdir();
-
-        System.out.println(String.format("Number of nodes: %d", numberOfNodes));
+        System.out.println(String.format("Writing exports to folder: %s", rootFolder));
+        new File(rootFolder).mkdir();
 
         TaskGraphGenerator taskGraphGenerator = new DefaultTaskGraphGenerator();
         try {
-            taskGraphGenerator.setNumberOfNodes(numberOfNodes);
             taskGraphGenerator.setMinIncomingEdges(minIncomgEdges);
             taskGraphGenerator.setMaxIncomingEdges(maxIncomingEdges);
             taskGraphGenerator.setSpreadEdges(spreadEdges);
@@ -61,15 +64,28 @@ public class Main {
 
         TaskGraphSerializer serializer = new STGSerializer();
 
-        for (int i = 0; i < numberOfGraphs; i++) {
-            taskGraphGenerator.generateSeed();
-            TaskGraph taskGraph = taskGraphGenerator.generator();
+        for (int i = 0; i < numberOfNodes.length; ++i) {
+            graphsFolder = Integer.toString(numberOfNodes[i]);
+            new File(rootFolder + "/" + graphsFolder).mkdir();
 
-            String pathname = String.format("%s/graph%d.stg", folderName, i);
             try {
-                TaskGraphFileUtils.writeFile(pathname, taskGraph, serializer);
-            } catch (IOException e) {
-                e.printStackTrace();
+                taskGraphGenerator.setNumberOfNodes(numberOfNodes[i]);
+            } catch (OutOfRangeException outOfRangeException) {
+                System.err.println("Missconfigured generator: " + outOfRangeException.getMessage());
+            } catch (GeneratorException generatorException) {
+                System.err.println("Missconfigured generator: " + generatorException.getMessage());
+            }
+
+            for (int j = 0; j < numberOfGraphs; j++) {
+                taskGraphGenerator.generateSeed();
+                TaskGraph taskGraph = taskGraphGenerator.generator();
+
+                String pathname = String.format("%s/%s/%d.stg", rootFolder, graphsFolder, j);
+                try {
+                    TaskGraphFileUtils.writeFile(pathname, taskGraph, serializer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
