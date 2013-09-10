188d9b3e67574136d1e40b682cbd68342870fd29
namelessvoid <pommesdiefritte@gmx.de>
2013-09-10 15:03:25 +0200

    Created rough DefaultStreamScheduler.

---
+++ src/com/github/noxan/blommagraphs/scheduling/impl/DefaultStreamScheduler.java
@@ -0,0 +1,24 @@
+package com.github.noxan.blommagraphs.scheduling.impl;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.impl.DefaultTaskGraph;
+import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
+import com.github.noxan.blommagraphs.scheduling.Scheduler;
+import com.github.noxan.blommagraphs.scheduling.StreamScheduler;
+import com.github.noxan.blommagraphs.scheduling.system.SystemMetaInformation;
+
+
+public class DefaultStreamScheduler implements StreamScheduler {
+
+    @Override
+    public ScheduledTaskList schedule(TaskGraph taskGraph, int graphCount,
+            SystemMetaInformation systemInfo, Scheduler scheduler) {
+
+        TaskGraph graph = new DefaultTaskGraph();
+        for (int i = 0; i < graphCount; ++i) {
+            graph.mergeGraph(taskGraph, graph.getFirstNode(), 1, graph.getLastNode(), 1);
+        }
+        return scheduler.schedule(graph, systemInfo);
+    }
+}
