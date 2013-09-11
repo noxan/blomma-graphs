8fa6e7b997052858d78939db334bfae3858e63c0
namelessvoid <pommesdiefritte@gmx.de>
2013-09-11 12:52:35 +0200

    Removed now unnecessary taskCount in StreamScheduler interface.

--- src/com/github/noxan/blommagraphs/scheduling/impl/DefaultStreamScheduler.java
+++ src/com/github/noxan/blommagraphs/scheduling/impl/DefaultStreamScheduler.java
@@ -12,11 +12,11 @@ import com.github.noxan.blommagraphs.scheduling.system.SystemMetaInformation;
 public class DefaultStreamScheduler implements StreamScheduler {
 
     @Override
-    public ScheduledTaskList schedule(TaskGraph taskGraph, int graphCount, int[] deadlines,
+    public ScheduledTaskList schedule(TaskGraph taskGraph, int[] deadLines,
             SystemMetaInformation systemInfo, Scheduler scheduler) {
 
         TaskGraph graph = new DefaultTaskGraph();
-        for (int i = 0; i < graphCount; ++i) {
+        for (int i = 0; i < deadLines.length; ++i) {
             graph.mergeGraph(taskGraph, graph.getFirstNode(), 1, graph.getLastNode(), 1);
         }
         return scheduler.schedule(graph, systemInfo);
