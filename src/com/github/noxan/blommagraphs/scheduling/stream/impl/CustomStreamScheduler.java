ca2f2362c34c4d0548dc106e3af5c238f6813c14
noxan <noxan@byteweaver.net>
2013-09-12 12:20:33 +0200

    let custom stream scheduler implement stream scheduler

--- src/com/github/noxan/blommagraphs/scheduling/stream/impl/CustomStreamScheduler.java
+++ src/com/github/noxan/blommagraphs/scheduling/stream/impl/CustomStreamScheduler.java
@@ -1,6 +1,18 @@
 package com.github.noxan.blommagraphs.scheduling.stream.impl;
 
 
-public class CustomStreamScheduler {
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
+import com.github.noxan.blommagraphs.scheduling.basic.Scheduler;
+import com.github.noxan.blommagraphs.scheduling.stream.StreamScheduler;
+import com.github.noxan.blommagraphs.scheduling.system.SystemMetaInformation;
 
+
+public class CustomStreamScheduler implements StreamScheduler {
+    @Override
+    public ScheduledTaskList schedule(TaskGraph[] taskGraphs, SystemMetaInformation systemInfo,
+            Scheduler scheduler) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
