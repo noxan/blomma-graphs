dfc3cc6a3c02093e8d16c900c4f56da712423207
namelessvoid <pommesdiefritte@gmx.de>
2013-09-13 14:03:41 +0200

    Removed useless GraphFactory interface.

--- src/com/github/noxan/blommagraphs/graphs/factories/GraphFactory.java
+++
@@ -1,9 +0,0 @@
-package com.github.noxan.blommagraphs.graphs.factories;
-
-
-import com.github.noxan.blommagraphs.graphs.TaskGraph;
-
-
-public interface GraphFactory {
-    TaskGraph makeGraph();
-}
