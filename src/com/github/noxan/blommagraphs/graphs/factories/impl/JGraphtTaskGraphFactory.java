784366d1bbc58012dde78e395f34977d253f72a2
namelessvoid <pommesdiefritte@gmx.de>
2013-09-13 13:59:08 +0200

    Implemented JGraphtTaskGraphFactory.makeGraph().

--- src/com/github/noxan/blommagraphs/graphs/factories/impl/JGraphtTaskGraphFactory.java
+++ src/com/github/noxan/blommagraphs/graphs/factories/impl/JGraphtTaskGraphFactory.java
@@ -2,6 +2,8 @@ package com.github.noxan.blommagraphs.graphs.factories.impl;
 
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+import com.github.noxan.blommagraphs.graphs.exceptions.DuplicateEdgeException;
 import com.github.noxan.blommagraphs.graphs.factories.GraphFactory;
 import com.github.noxan.blommagraphs.graphs.impl.JGraphtTaskGraph;
 
@@ -10,6 +12,26 @@ public class JGraphtTaskGraphFactory implements GraphFactory {
     @Override
     public TaskGraph makeGraph() {
         TaskGraph taskGraph = new JGraphtTaskGraph();
+        TaskGraphNode nodes[] = new TaskGraphNode[10];
+
+        nodes[0] = taskGraph.getFirstNode();
+        nodes[9] = taskGraph.getLastNode();
+
+        nodes[1] = taskGraph.insertNode(nodes[0], 1, nodes[9], 1, 10);
+        nodes[2] = taskGraph.insertNode(nodes[1], 5, nodes[9], 4, 5);
+        nodes[3] = taskGraph.insertNode(nodes[0], 1, nodes[2], 5, 10);
+        nodes[4] = taskGraph.insertNode(nodes[0], 1, nodes[9], 8, 15);
+        nodes[5] = taskGraph.insertNode(nodes[3], 6, nodes[9], 3, 15);
+        nodes[6] = taskGraph.insertNode(nodes[2], 4, nodes[9], 1, 10);
+        nodes[7] = taskGraph.insertNode(nodes[1], 3, nodes[6], 4, 20);
+        nodes[8] = taskGraph.insertNode(nodes[5], 3, nodes[9], 1, 7);
+
+        try {
+            taskGraph.insertEdge(nodes[0], nodes[2], 7);
+            taskGraph.insertEdge(nodes[2], nodes[8], 6);
+        } catch (DuplicateEdgeException e) {
+            e.printStackTrace();
+        }
 
         return taskGraph;
     }
