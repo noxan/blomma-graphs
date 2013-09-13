e69785d853d5a6e5113b4ceedbc13f041d73bf3a
namelessvoid <pommesdiefritte@gmx.de>
2013-09-13 12:28:48 +0200

    Created GraphFactory interface.

---
+++ src/com/github/noxan/blommagraphs/graphs/factories/GraphFactory.java
@@ -0,0 +1,9 @@
+package com.github.noxan.blommagraphs.graphs.factories;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+
+
+public interface GraphFactory {
+    TaskGraph makeGraph();
+}
