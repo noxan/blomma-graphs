939afb92e8ace611040a9409f8b5b21befbd8297
namelessvoid <pommesdiefritte@gmx.de>
2013-09-14 15:10:08 +0200

    Created StreamSchedulableArrayGenerator.

---
+++ src/com/github/noxan/blommagraphs/utils/StreamSchedulableArrayGenerator.java
@@ -0,0 +1,16 @@
+package com.github.noxan.blommagraphs.utils;
+
+
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+
+
+public class StreamSchedulableArrayGenerator {
+
+    /**
+     * @param args
+     */
+    public TaskGraph[] generateArray(TaskGraph graph, int[] deadLines) {
+        return null;
+    }
+
+}
