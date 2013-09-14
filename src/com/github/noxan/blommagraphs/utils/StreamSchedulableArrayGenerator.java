cb623c941ec7c9cf57538ecf6ea7ded3c819da74
namelessvoid <pommesdiefritte@gmx.de>
2013-09-14 15:17:09 +0200

    Updated javadoc and commentaries for StreamSchedulableArrayGenerator.

--- src/com/github/noxan/blommagraphs/utils/StreamSchedulableArrayGenerator.java
+++ src/com/github/noxan/blommagraphs/utils/StreamSchedulableArrayGenerator.java
@@ -6,16 +6,32 @@ import com.github.noxan.blommagraphs.graphs.serializer.TaskGraphSerializer;
 import com.github.noxan.blommagraphs.graphs.serializer.impl.STGSerializer;
 
 
+/**
+ * This class is used to create an array of TaskGraphs that can be used for scheduling by a
+ * StreamScheduler.
+ * 
+ * @author namelessvoid
+ * 
+ */
 public class StreamSchedulableArrayGenerator {
 
     /**
-     * @param args
+     * This method gets one single TaskGraph as input and multiplies it. It creates as many new
+     * graphs as the number of deadlines in the deadLines array. Deadlines for each TaskGraph are
+     * reset by the deadLines. The resulting TaskGraph array can now easily be passed to an
+     * StreamScheduler which requires an array of TaskGraphs to schedule them.
+     * 
+     * @param graph One instance of a TaskGraph that is multiplied.
+     * @param deadLines Determines a deadline for each new TaskGraph instance and also how many
+     *            graphs are generated (deadLines.length).
+     * 
+     * @return An array that holds newly generated TaskGraphs.
      */
     public TaskGraph[] generateArray(TaskGraph graph, int[] deadLines) {
         TaskGraph graphs[] = new TaskGraph[deadLines.length];
 
-        // Ugly workaround: use serialize -> deserialize to get new graph instance. That's supposed
-        // to be replaced when a deep-copy method for TaskGraph is implemented.
+        // Ugly workaround: TODO use serialize -> deserialize to get new graph instance. That's
+        // supposed to be replaced when a deep-copy method for TaskGraph is implemented.
         TaskGraphSerializer serializer = new STGSerializer();
         String serializedGraph = serializer.serialize(graph);
 
