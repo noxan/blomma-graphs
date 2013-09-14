6147da9a1c38e8327928092bdabc7d0fa594b749
namelessvoid <pommesdiefritte@gmx.de>
2013-09-14 15:10:34 +0200

    Implemented StreamSchedulableArrayGenerator.generateArray().

--- src/com/github/noxan/blommagraphs/utils/StreamSchedulableArrayGenerator.java
+++ src/com/github/noxan/blommagraphs/utils/StreamSchedulableArrayGenerator.java
@@ -2,6 +2,8 @@ package com.github.noxan.blommagraphs.utils;
 
 
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.serializer.TaskGraphSerializer;
+import com.github.noxan.blommagraphs.graphs.serializer.impl.STGSerializer;
 
 
 public class StreamSchedulableArrayGenerator {
@@ -10,7 +12,19 @@ public class StreamSchedulableArrayGenerator {
      * @param args
      */
     public TaskGraph[] generateArray(TaskGraph graph, int[] deadLines) {
-        return null;
+        TaskGraph graphs[] = new TaskGraph[deadLines.length];
+
+        // Ugly workaround: use serialize -> deserialize to get new graph instance. That's supposed
+        // to be replaced when a deep-copy method for TaskGraph is implemented.
+        TaskGraphSerializer serializer = new STGSerializer();
+        String serializedGraph = serializer.serialize(graph);
+
+        for (int i = 0; i < deadLines.length; ++i) {
+            graphs[i] = serializer.deserialize(serializedGraph);
+            graphs[i].resetDeadLine(deadLines[i]);
+        }
+
+        return graphs;
     }
 
 }
