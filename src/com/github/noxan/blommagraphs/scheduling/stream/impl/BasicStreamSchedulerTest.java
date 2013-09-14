c369a6217ff9396c6193f2434dd1144df08cbe57
namelessvoid <pommesdiefritte@gmx.de>
2013-09-14 16:41:52 +0200

    Created test class for BasicStreamScheduler.

---
+++ src/com/github/noxan/blommagraphs/scheduling/stream/impl/BasicStreamSchedulerTest.java
@@ -0,0 +1,19 @@
+package com.github.noxan.blommagraphs.scheduling.stream.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicStreamSchedulerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testSchedule() {
+        fail("Not yet implemented");
+    }
+
+}
