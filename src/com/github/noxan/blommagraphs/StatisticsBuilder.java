20898bf00da0d9cb80e62221adede326efa484bb
namelessvoid <pommesdiefritte@gmx.de>
2013-09-18 21:16:40 +0200

    Created StatisticsBuilder.

---
+++ src/com/github/noxan/blommagraphs/StatisticsBuilder.java
@@ -0,0 +1,13 @@
+package com.github.noxan.blommagraphs;
+
+public class StatisticsBuilder {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
