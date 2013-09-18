ca99c5da70ba2c7896afe9a351e4f1ac4ca6eaf8
namelessvoid <pommesdiefritte@gmx.de>
2013-09-18 22:14:31 +0200

    Added lists to save evaluation values and constructor to StatisticsBuilder.

--- src/com/github/noxan/blommagraphs/StatisticsBuilder.java
+++ src/com/github/noxan/blommagraphs/StatisticsBuilder.java
@@ -1,6 +1,19 @@
 package com.github.noxan.blommagraphs;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class StatisticsBuilder {
+
+    private final int schedulerCount = 4;
+    private final int taskGraphCount = 500;
+    private final int taskGroupCount = 5;
+
+    private List<List<List<Float>>> taskGraphStatistics;
+    private List<List<List<Float>>> taskGroupStatistics;
+    private List<List<Float>> schedulerStatistics;
 
     /**
      * @param args
@@ -10,4 +23,24 @@ public class StatisticsBuilder {
 
     }
 
+    private StatisticsBuilder() {
+        // Build lists
+        taskGraphStatistics = new ArrayList<List<List<Float>>>();
+        taskGroupStatistics = new ArrayList<List<List<Float>>>();
+        schedulerStatistics = new ArrayList<List<Float>>();
+
+        for (int scheduler = 0; scheduler < schedulerCount; ++scheduler) {
+            taskGraphStatistics.add(new ArrayList<List<Float>>());
+            taskGroupStatistics.add(new ArrayList<List<Float>>());
+            schedulerStatistics.add(new ArrayList<Float>());
+
+            for (int taskGraph = 0; taskGraph < taskGraphCount; ++taskGraph) {
+                taskGraphStatistics.get(scheduler).add(new ArrayList<Float>());
+            }
+            for (int taskGroup = 0; taskGroup < taskGroupCount; ++taskGroup) {
+                taskGroupStatistics.get(scheduler).add(new ArrayList<Float>());
+            }
+        }
+
+    }
 }
