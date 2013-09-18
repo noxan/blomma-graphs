6383a0210190522504985aa2ab2cfe63d9a19d56
namelessvoid <pommesdiefritte@gmx.de>
2013-09-18 22:53:11 +0200

    Added IOException to StatisticsBuilder.main().

--- src/com/github/noxan/blommagraphs/StatisticsBuilder.java
+++ src/com/github/noxan/blommagraphs/StatisticsBuilder.java
@@ -42,7 +42,7 @@ public class StatisticsBuilder {
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
 
     }
