5fe81c619fb2e97bfe2140021db18867362a8cd9
noxan <noxan@byteweaver.net>
2013-09-19 14:37:54 +0200

    add dummy test case for validate method in ScheduledTaskList (#46)

--- test/com/github/noxan/blommagraphs/scheduling/impl/DefaultScheduledTaskListTest.java
+++ test/com/github/noxan/blommagraphs/scheduling/impl/DefaultScheduledTaskListTest.java
@@ -1,6 +1,22 @@
 package com.github.noxan.blommagraphs.scheduling.impl;
 
 
-public class DefaultScheduledTaskListTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
+
+
+public class DefaultScheduledTaskListTest {
+    private ScheduledTaskList scheduledTaskList;
+
+    @Before
+    public void initialize() {
+        scheduledTaskList = new DefaultScheduledTaskList(4);
+    }
+
+    @Test
+    public void testValidate() {
+        scheduledTaskList.validate();
+    }
 }
