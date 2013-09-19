5f02642a5025b1873b91e127ebb58d9fe0eb984b
noxan <noxan@byteweaver.net>
2013-09-19 14:47:54 +0200

    add dummy data to default scheduled task list

--- test/com/github/noxan/blommagraphs/scheduling/impl/DefaultScheduledTaskListTest.java
+++ test/com/github/noxan/blommagraphs/scheduling/impl/DefaultScheduledTaskListTest.java
@@ -4,15 +4,28 @@ package com.github.noxan.blommagraphs.scheduling.impl;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.noxan.blommagraphs.graphs.TaskGraph;
+import com.github.noxan.blommagraphs.graphs.TaskGraphNode;
+import com.github.noxan.blommagraphs.graphs.impl.DefaultTaskGraph;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
 
 
 public class DefaultScheduledTaskListTest {
+    private TaskGraph taskGraph;
     private ScheduledTaskList scheduledTaskList;
 
     @Before
     public void initialize() {
-        scheduledTaskList = new DefaultScheduledTaskList(4);
+        taskGraph = new DefaultTaskGraph();
+        TaskGraphNode task0 = taskGraph.getFirstNode();
+        TaskGraphNode task3 = taskGraph.getLastNode();
+        TaskGraphNode task1 = taskGraph.insertNode(task0, 3, task3, 5, 4);
+        TaskGraphNode task2 = taskGraph.insertNode(task0, 5, task3, 2, 8);
+        scheduledTaskList = new DefaultScheduledTaskList(2);
+        scheduledTaskList.add(new DefaultScheduledTask(0, 0, 0, task0));
+        scheduledTaskList.add(new DefaultScheduledTask(1, 0, 0, task1));
+        scheduledTaskList.add(new DefaultScheduledTask(1, 1, 0, task2));
+        scheduledTaskList.add(new DefaultScheduledTask(9, 0, 0, task3));
     }
 
     @Test
