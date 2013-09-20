fe9183e2740bcb03eafa297cd0fd67f99004e21b
noxan <noxan@byteweaver.net>
2013-09-20 11:19:45 +0200

    rename pratical evaluator and interface to simulator

---
+++ src/com/github/noxan/blommagraphs/evaluation/impl/PracticalScheduleSimulator.java
@@ -0,0 +1,8 @@
+package com.github.noxan.blommagraphs.evaluation.impl;
+
+
+import com.github.noxan.blommagraphs.evaluation.ScheduleSimulator;
+
+
+public class PracticalScheduleSimulator implements ScheduleSimulator {
+}
--- src/com/github/noxan/blommagraphs/evaluation/impl/PracticalSchedulingEvaluator.java
+++
@@ -1,9 +0,0 @@
-package com.github.noxan.blommagraphs.evaluation.impl;
-
-
-import com.github.noxan.blommagraphs.evaluation.SchedulingEvaluator;
-
-
-public class PracticalSchedulingEvaluator implements SchedulingEvaluator {
-
-}
