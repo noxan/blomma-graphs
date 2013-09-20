fdc153f234a7ac57b92f2bc74410296256dbcac7
noxan <noxan@byteweaver.net>
2013-09-20 11:31:42 +0200

    create a schedule simulation worker for each cpu

--- src/com/github/noxan/blommagraphs/evaluation/impl/PracticalScheduleSimulator.java
+++ src/com/github/noxan/blommagraphs/evaluation/impl/PracticalScheduleSimulator.java
@@ -1,13 +1,25 @@
 package com.github.noxan.blommagraphs.evaluation.impl;
 
 
+import com.github.noxan.blommagraphs.evaluation.ScheduleSimulationWorker;
 import com.github.noxan.blommagraphs.evaluation.ScheduleSimulator;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
 
 
 public class PracticalScheduleSimulator implements ScheduleSimulator {
+    private ScheduleSimulationWorker[] workers;
+
+    public PracticalScheduleSimulator() {
+    }
+
     @Override
     public ScheduledTaskList simulateExecution(ScheduledTaskList scheduledTaskList) {
+        workers = new ScheduleSimulationWorker[scheduledTaskList.getCpuCount()];
+
+        for (int cpuId = 0; cpuId < workers.length; cpuId++) {
+            workers[cpuId] = new TimebasedScheduleSimulationWorker();
+        }
+
         // TODO Auto-generated method stub
         return null;
     }
