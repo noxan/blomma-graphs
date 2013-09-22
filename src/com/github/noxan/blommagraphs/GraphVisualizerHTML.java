0e75a8d8768709661293a11b31d9e4bb7f3ba9eb
ruction <benjaminwoehrl@googlemail.com>
2013-09-22 15:33:28 +0200

    Adjusted styling in HTMLSerializer.

--- src/com/github/noxan/blommagraphs/GraphVisualizerHTML.java
+++ src/com/github/noxan/blommagraphs/GraphVisualizerHTML.java
@@ -1,14 +1,8 @@
 package com.github.noxan.blommagraphs;
 
 
-import java.io.IOException;
-
-import com.github.noxan.blommagraphs.generator.TaskGraphGenerator;
-import com.github.noxan.blommagraphs.generator.impl.DefaultTaskGraphGenerator;
 import com.github.noxan.blommagraphs.graphs.TaskGraph;
 import com.github.noxan.blommagraphs.graphs.factories.impl.DefaultTaskGraphFactory;
-import com.github.noxan.blommagraphs.graphs.serializer.TaskGraphSerializer;
-import com.github.noxan.blommagraphs.graphs.serializer.impl.STGSerializer;
 import com.github.noxan.blommagraphs.scheduling.ScheduledTaskList;
 import com.github.noxan.blommagraphs.scheduling.basic.Scheduler;
 import com.github.noxan.blommagraphs.scheduling.basic.impl.last.LASTScheduler;
@@ -17,6 +11,8 @@ import com.github.noxan.blommagraphs.scheduling.serializer.impl.HTMLSerializer;
 import com.github.noxan.blommagraphs.scheduling.system.SystemMetaInformation;
 import com.github.noxan.blommagraphs.scheduling.system.impl.DefaultSystemMetaInformation;
 import com.github.noxan.blommagraphs.utils.FileUtils;
+
+import java.io.IOException;
 
 
 public class GraphVisualizerHTML {
