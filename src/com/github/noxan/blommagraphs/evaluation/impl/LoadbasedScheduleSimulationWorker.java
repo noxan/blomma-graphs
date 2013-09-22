30c8564cdadad9d0b47f19b625f56f2be5594e9d
noxan <noxan@byteweaver.net>
2013-09-22 19:53:43 +0200

    add first approach to a load based  schedule simulator

--- src/com/github/noxan/blommagraphs/evaluation/impl/LoadbasedScheduleSimulationWorker.java
+++ src/com/github/noxan/blommagraphs/evaluation/impl/LoadbasedScheduleSimulationWorker.java
@@ -7,7 +7,9 @@ import com.github.noxan.blommagraphs.evaluation.ScheduleSimulationWorker;
 public class LoadbasedScheduleSimulationWorker implements ScheduleSimulationWorker {
     @Override
     public void work(int workTime) {
-        // TODO Auto-generated method stub
-
+        for (int i = 0; i < workTime * 50; i++) {
+            for (int j = Integer.MIN_VALUE; j < Integer.MAX_VALUE; j++) {
+            }
+        }
     }
 }
