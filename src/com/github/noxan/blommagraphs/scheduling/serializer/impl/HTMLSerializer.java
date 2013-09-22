7c5535b875c85ddcb195518dcb0f9bb1d632e1d0
ruction <benjaminwoehrl@googlemail.com>
2013-09-22 13:27:04 +0200

    Optimized GraphVisualizerHTML.

--- src/com/github/noxan/blommagraphs/scheduling/serializer/impl/HTMLSerializer.java
+++ src/com/github/noxan/blommagraphs/scheduling/serializer/impl/HTMLSerializer.java
@@ -9,13 +9,18 @@ public class HTMLSerializer implements ScheduledTaskListSerializer {
 
     @Override
     public String serialize(ScheduledTaskList scheduledTaskList) {
-        StringBuffer string = new StringBuffer();
-        string.append("<html>\n<head><title>\nHTML Serializer"
-                + "\n</title>\n<link rel=\"stylesheet\" href=\"styleshit.css\""
-                + "type=\"text/css\" media=\"screen\" />\n</head>\n<body>\n");
+        StringBuilder string = new StringBuilder();
+        string.append("<html>\n");
+        string.append(" <head>\n");
+        string.append("     <title>HTML Serializer</title>\n");
+        string.append("     <link rel=\"stylesheet\" href=\"styleshit.css\" type=\"text/css\" media=\"screen\" />\n");
+        string.append(" </head>\n");
+        string.append(" <body>\n");
+
         for (int i = 0; i < scheduledTaskList.getCpuCount(); i++) {
             System.out.println("CPU COUNT" + scheduledTaskList.getCpuCount());
-            string.append("<div id=p" + "" + (i + 1) + ">" + "\n");
+
+            string.append("<div id=\"p").append(i + 1).append("\">\n");
             boolean bitch = true;
             int counti = 1;
             int flummi = 1;
@@ -24,15 +29,14 @@ public class HTMLSerializer implements ScheduledTaskListSerializer {
                         .getScheduledTasksOnCpu(i);
 
                 if (bitch) {
-                    string.append("<div class=header>\n<h1" + ">Processor " + (i + 1) + "</h1"
-                            + ">\n</div>\n");
+                    string.append("<div class=\"header\">\n");
+                    string.append(" <h1>Processor ").append(i + 1).append("</h1>\n");
+                    string.append("</div><!-- header -->\n");
                     bitch = false;
                 }
                 flummi = j;
                 if (j == 0 && i != 0) {
-                    string.append("<div class=waitTime " + "style=\"height: "
-                            + scheduledTaskOnCpuList.get(j).getStartTime() * 10 + "px;\"></div>\n");
-
+                    string.append("<div class=\"waitTime\" style=\"height: ").append(scheduledTaskOnCpuList.get(j).getStartTime() * 10).append("px;\"></div><!-- waittime -->\n");
                 }
                 int waitTime = 0;
                 if (j == 0) {
@@ -55,25 +59,20 @@ public class HTMLSerializer implements ScheduledTaskListSerializer {
                 counti += 1;
 
                 if (waitTime > 0) {
-                    string.append("<div class=waitTime " + "style=\"height: " + waitTime * 10
-                            + "px;\"></div>\n");
+                    string.append("<div class=\"waitTime\" style=\"height: ").append(waitTime * 10).append("px;\"></div><!-- waittime -->\n");
                     waitTime = 0;
                 }
 
                 if (scheduledTaskOnCpuList.get(j).getCommunicationTime() != 0) {
-                    string.append("<div class=gap "
-                            + "style=\"height: "
-                            + scheduledTaskList.getScheduledTasksOnCpu(i).getTaskById(j)
-                                    .getCommunicationTime() * 10 + "px;\"></div>\n");
+                    string.append("<div class=\"gap\" style=\"height: ").append(scheduledTaskList.getScheduledTasksOnCpu(i).getTaskById(j).getCommunicationTime() * 10).append("px;\"></div><!-- gap -->\n");
                 }
-                string.append("<div class=pd" + (i + 1) + " style=\" width: 200px; height: "
-                        + scheduledTaskOnCpuList.get(j).getComputationTime() * 10 + "px\";" + "\""
-                        + ">\n<p>Task " + scheduledTaskOnCpuList.get(j).getTaskId()
-                        + "</p>\n</div>" + "\n");
+                string.append("<div class=\"pd").append(i + 1).append("\" style=\" width: 200px; height: ").append(scheduledTaskOnCpuList.get(j).getComputationTime() * 10).append("px;\">\n");
+                string.append(" <p>Task ").append(scheduledTaskOnCpuList.get(j).getTaskId()).append("</p>\n");
+                string.append("</div>\n");
 
             }
 
-            string.append("</div>" + "\n");
+            string.append("</div><!-- processor n -->\n");
         }
 
         return string.toString();
